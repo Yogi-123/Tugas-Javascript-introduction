4d934c41cbae0124b628886b0276579d836c3622
Yogi-123 <myogi.jp@gmail.com>
2020-08-18 09:17:51 +0700

    mengisi tugas

---
+++ index.js
@@ -0,0 +1,69 @@
+// console log data diri
+let nama = "muhammad yogi";
+let asalDaerah = "Bukittinggi";
+let tanggalLahir = "2 februari 1996";
+let umur = "24 tahun";
+let posisiPekerjaan = "belum bekerja";
+
+console.log(nama)
+console.log(asalDaerah)
+console.log(tanggalLahir)
+console.log(umur)
+console.log(posisiPekerjaan)
+
+// membuat interaksi kepada user
+prompt("nama user")
+prompt("asal negara")
+confirm("apakah data yang diisi sudah benar  ?")
+alert("terimakasih sudah mengisi form")
+
+// single comment
+console.log(nama)
+console.log(asalDaerah)
+console.log(tanggalLahir)
+console.log(umur)
+console.log(posisiPekerjaan)
+
+// multiple comment soal no 2
+
+// variabel untuk menampung data diri
+let dataDiri = {
+    nama: "muhammad yogi",
+    asalDaerah: "Bukittinggi",
+    tanggalLahir: "2 februari 1996",
+    umur: "24 tahun",
+    posisiPekerjaan: "belum bekerja"
+  
+};  
+
+console.log(dataDiri.nama)
+console.log(dataDiri.asalDaerah)
+console.log(dataDiri.tanggalLahir)
+console.log(dataDiri.umur)
+console.log(dataDiri.posisiPekerjaan)
+
+// operasi matematika
+
+var a, b, hasil;
+a = 25
+b = 10
+hasil = a + b;
+console.log(hasil)
+
+var a, b, hasil;
+a = 100
+b = 2
+hasil = a * b;
+console.log(hasil)
+
+var a, b, hasil;
+a = 99
+b = 2
+hasil = a / b;
+console.log(hasil)
+
+var a, b, hasil;
+a = 99
+b = 2
+hasil = a % b;
+console.log(hasil)
